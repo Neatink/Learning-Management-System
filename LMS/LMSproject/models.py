c92214c681b0fc27bc541c07635bc3fa5eaebcca
Neatink <minyaylo51@gmail.com>
2025-08-01 12:08:37 +0300

    Add new models(Lesson,Course)

--- LMS/LMSproject/models.py
+++ LMS/LMSproject/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    task = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    video = models.FileField(null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
