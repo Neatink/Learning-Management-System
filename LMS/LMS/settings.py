0a32bc2c49b320f2b23a795653410ccff143d14d
Neatink <minyaylo51@gmail.com>
2025-08-07 18:23:44 +0300

    Add logout

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -126,4 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/home'
 
+LOGOUT_REDIRECT_URL = '/home'
+
 LOGIN_URL = '/login'
--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('profile/<pk>', ProfileView.as_view(), name='profile_view'),
     path('user_change_data/<pk>', ChangeUserDataView.as_view(), name='user_change_data_view'),
     path('user_change_password/', ChangeUserPasswordView.as_view(), name='user_change_password_view'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
