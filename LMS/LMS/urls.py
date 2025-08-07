4b5093651ac691a880a581f9b9af300409ce0ffb
Neatink <minyaylo51@gmail.com>
2025-08-07 16:06:11 +0300

    Add login to account

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from LMSproject.views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', HomeView.as_view(), name='homeview'),
     path('course/detail/<pk>', CoursesDetailView.as_view(), name='coursedetail'),
     path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
