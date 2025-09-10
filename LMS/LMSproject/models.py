f52d8c39dd2905459b9b6a5f2db7c6b36935dd68
Neatink <minyaylo51@gmail.com>
2025-09-10 21:12:48 +0300

    Add banner with courses

--- LMS/LMSproject/models.py
+++ LMS/LMSproject/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Course(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
